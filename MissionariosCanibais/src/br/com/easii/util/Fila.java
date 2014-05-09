0a7fca616d34a581f12b42551c3baa0be04ec41b
Wermeson <wermesonhot@hotmail.com>
2014-05-09 09:10:18 -0300

    implementações das estruturas pilha e fila.

--- MissionariosCanibais/src/br/com/easii/util/Fila.java
+++ MissionariosCanibais/src/br/com/easii/util/Fila.java
@@ -3,10 +3,28 @@
  */
 package br.com.easii.util;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @author Ronyerison
  *
  */
 public class Fila {
-
+	private List<Object> objetos = new LinkedList<Object>();
+	
+	public void inserir(Object objeto){
+		this.objetos.add(objeto);
+	}
+	
+	public Object remover(){
+		return this.objetos.remove(0);
+	}
+	
+	public boolean vazia(){
+		if(this.objetos.size() == 0){
+			return true;
+		}
+		return false;
+	}
 }
--- MissionariosCanibais/src/br/com/easii/util/Pilha.java
+++ MissionariosCanibais/src/br/com/easii/util/Pilha.java
@@ -2,11 +2,32 @@
  * 
  */
 package br.com.easii.util;
-
+import java.util.*;
 /**
  * @author Ronyerison
  *
  */
-public class Pilha {
 
+public class Pilha {
+	List<Object> objetos = new LinkedList<Object>();
+	
+	public void empilhar(Object object){
+		this.objetos.add(object);
+	}
+	
+	public Object desempilhar(){
+		return this.objetos.remove(this.objetos.size()-1);
+	}
+	
+	public boolean vazia(){
+		if(this.objetos.size() == 0){
+			return true;
+		}
+		return false;
+	}
+	
+//	top -> retorna o ultimo elemento inserido sem removê-lo
+	public Object top(){
+		return this.objetos.get(this.objetos.size()-1);
+	}
 }
